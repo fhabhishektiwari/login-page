ed936a942d35dac35137ac596962313eb5981727
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-05-12 10:47:17 +0530

    addig files

---
+++ header.php
@@ -0,0 +1,45 @@
+<?php
+    include('connection.php');
+    
+    
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang='en-IN'>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h3>
+                <a href="index.html">Wellcare</a>
+            </h3>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul> 
+            </nav>
+        </header>
+        
+        
+        <section id="banner">
+            <h1>Wellcome to Welcare!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            
+        </section>
+    
+    
+    </body>
+    
+</html>
